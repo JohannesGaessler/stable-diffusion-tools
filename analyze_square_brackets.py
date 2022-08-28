2cb8d085f31f136fc1b7d987279d63dad625f92c
JohannesGaessler <johannesg@5d6.de>
2022-08-28 09:37:22 +0200

    Revised uncertainty model

--- analyze_square_brackets.py
+++ analyze_square_brackets.py
@@ -15,23 +15,30 @@ PROMPTS = [
     "Flowers, [[[[[red]]]]], blue",
 ]
 
-x_data = np.arange(len(PROMPTS))
+x_data = np.arange(1, len(PROMPTS))
 y_data = []
 y_error = []
 
-for prompt in PROMPTS:
+for i, prompt in enumerate(PROMPTS):
     prompt_dir = os.path.join(SAMPLE_DIR, prompt)
-    means = []
+    means_i = []
     file_list = os.listdir(prompt_dir)
     num_samples = len(file_list)
+
     for filename in file_list:
         if not filename.endswith(".png"):
             continue
         image = iio.v3.imread(os.path.join(prompt_dir, filename))
         red = np.mean(image[:, :, 0])
-        means.append(red)
-    y_data.append(np.mean(means))
-    y_error.append(np.std(means) / np.sqrt(num_samples))
+        means_i.append(red)
+
+    means_i = np.array(means_i)
+    if i == 0:
+        means_0 = means_i
+    else:
+        diffs = means_i - means_0
+        y_data.append(np.mean(diffs))
+        y_error.append(np.std(diffs) / np.sqrt(num_samples))
 
 
 fit = XYFit([x_data, y_data])
@@ -40,6 +47,7 @@ fit.do_fit()
 
 plot = Plot(fit)
 plot.x_label = "Number of square  brackets PRELIMINARY RESULT"
-plot.y_label = "Mean red pixel value PRELIMINARY RESULT"
+plot.y_label = "Mean red pixel value diff PRELIMINARY RESULT"
 plot.plot()
+plot.save("square_brackets.png", dpi=320)
 plot.show()
